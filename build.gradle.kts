e202c4224358d6de58b6cf624afb6c2df6852d12
KChi <59391215+KagChi@users.noreply.github.com>
2022-06-05 12:42:11 +0700

    feat: initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    application
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "org.nezu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "13"
+}
+
+application {
+    mainClass.set("org.nezu.Main")
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes["Main-Class"] = "org.nezu.Main"
+    }
+
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
---
+++ src/main/kotlin/org/nezu/Main.kt
@@ -0,0 +1,7 @@
+class Main {
+    companion object {
+         @JvmStatic fun main(args: Array<String>) {
+                println("鬼を捕まえる準備ができました !")
+         }
+    }
+}
